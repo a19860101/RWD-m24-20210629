bc1b8ee9e16d290a4714651376222fc6500b3fa8
Zac <a19860101@gmail.com>
2021-08-12 10:22:35 +0800

    20210812

--- JAVASCRIPT/basic.js
+++ JAVASCRIPT/basic.js
@@ -1,1 +1,43 @@
-alert('hello');
+// 變數 variable
+
+// var a = 100;
+// var b;
+// console.log(b);
+
+
+let a = 123;
+a = 999;
+console.log(a);
+
+// 常數 constant variable
+const x = 333;
+console.log(x);
+
+// 常數不可重新賦值
+
+
+/* 
+    var vs let,const
+    1. let,const 不可重複宣告
+    2. var 是function scope,let,const是block scope
+
+*/
+
+let s = 'hello';
+let b = null;
+console.log(b)
+
+/* 
+    資料型態 
+    1. 數值 Number
+    2. 字串 String
+    3. 布林 Boolean
+    4. 未定義 Undefined
+    5. 空值 Null
+    6. 陣列 Array
+    7. 物件 Object
+    8. 函式 Function
+
+
+*/
+
