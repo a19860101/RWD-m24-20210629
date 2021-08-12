fb79341ff79888aa4c270d950df2143da56b90c6
Zac <a19860101@gmail.com>
2021-08-12 11:41:09 +0800

    20210812

--- JAVASCRIPT/condition.js
+++ JAVASCRIPT/condition.js
@@ -1,3 +1,39 @@
 // if
-let x = 10;
-if(){}
+let x = 0;
+if(x > 0){
+    console.log('success');
+}
+
+
+// if...else
+
+if(x >0) {
+    console.log('success');
+}else {
+    console.log('error');
+}
+
+// if...else if...else
+
+if(x > 0){
+    console.log('正');
+}else if(x < 0){
+    console.log('負');
+}else{
+    console.log('error');
+}
+
+// switch
+switch(x){
+    case 0:
+        console.log(0);
+        // break;
+    case 10:
+        console.log(10);
+        // break;
+    case 100:
+        console.log(100);
+        // break;
+    default:
+        console.log('ERROR');
+}
