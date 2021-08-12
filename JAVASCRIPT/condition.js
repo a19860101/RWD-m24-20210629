f2f86184b15a88935f5767109b962b0b41f51907
Zac <a19860101@gmail.com>
2021-08-12 11:53:32 +0800

    20210812

--- JAVASCRIPT/condition.js
+++ JAVASCRIPT/condition.js
@@ -1,9 +1,16 @@
 // if
-let x = 0;
+let x = 10;
 if(x > 0){
     console.log('success');
 }
 
+// console.log( x > 0 ? 'success!!':'error!!');
+// 條件 ? 成立 : 不成立
+
+console.log( x > 0 ? '正!!': x < 0 ? '負!!':'error!!!');
+// 條件 ? 成立 : 條件二 ? 成立 : 不成立 
+let result = x > 0 ? '正!!': x < 0 ? '負!!':'error!!!';
+console.log(result);
 
 // if...else
 
@@ -27,13 +34,24 @@ if(x > 0){
 switch(x){
     case 0:
         console.log(0);
-        // break;
+        break;
     case 10:
         console.log(10);
-        // break;
+        break;
     case 100:
         console.log(100);
-        // break;
+        break;
     default:
         console.log('ERROR');
+}
+
+switch(true){
+    case x>0:
+        console.log('正數');
+        break;
+    case x<0:
+        console.log('負數');
+        break;
+    default:
+        console.log(0);
 }
