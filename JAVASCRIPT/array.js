98b7700e4f83caceb0f6afd0154d3f593e09064a
Zac <a19860101@gmail.com>
2021-08-17 09:34:52 +0800

    20210817

---
+++ JAVASCRIPT/array.js
@@ -0,0 +1,38 @@
+// 陣列 Array
+
+// let a = new Array();
+// a[0] = 'Apple';
+// a[1] = 'Banana';
+// a[2] = 'Cat';
+
+// let a = new Array('Apple','Banana','Cat');
+let a = ['Apple','Banana','Cat','Dog'];
+
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+
+console.log(a.length);
+
+// 陣列迭代
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+// for(let data of a){
+//     console.log(data);
+// }
+
+// for(let i in a){
+//     console.log(a[i]);
+// }
+
+// push(), pop()
+a.push('Egg');
+a.pop();
+
+// unshift(), shift()
+a.unshift('House');
+a.shift();
+console.log(a);
---
+++ JAVASCRIPT/loop.js
@@ -0,0 +1,16 @@
+// 迴圈
+// for
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// for...of
+// for...in
+
+// while
+let j = 0;
+while(j < 10){
+    j++;
+    console.log(j);
+}
