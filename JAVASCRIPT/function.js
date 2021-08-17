f7b9a57ed6c818d5509e11d1691afdb87a4a763a
Zac <a19860101@gmail.com>
2021-08-17 12:24:42 +0800

    20210817

--- JAVASCRIPT/function.js
+++ JAVASCRIPT/function.js
@@ -25,3 +25,53 @@ function us(price,exchange){
     return price * exchange;
 }
  console.log(us(100,28));
+
+// 箭頭函式
+// let a = function(){
+//     return 100;
+// }
+
+// let a = () => {
+//     return 100;
+// }
+
+// let a = x => {
+//     return x;
+// }
+
+// let a = (x,y) => {
+//     return x * y;
+// }
+
+// let a = () => 100;
+// let a = x => x;
+let a = (x,y) => x * y;
+
+console.log(a(123,3221));
+
+let drinks = [
+    {
+        id:1,
+        name: '紅茶',
+        price: '20'
+    },
+    {
+        id:2,
+        name: '綠茶',
+        price: '20'
+    },
+    {
+        id:3,
+        name: '奶茶',
+        price: '30'
+    }
+];
+
+let drinks_name = drinks.map( data => data.name );
+console.log(drinks_name);
+
+let drinks_price = drinks.map( data => data.price);
+console.log(drinks_price);
+
+let total = drinks.map( data => data.price).reduce((a,b) => Number(a) + Number(b));
+console.log(total);
