2d860d21cf5a2f08a1537a046e25f2c1c794b3e7
Zac <a19860101@gmail.com>
2021-08-17 11:46:51 +0800

    20210817

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -11,5 +11,30 @@ let user2 = {
 
 
 // console.log(user,user2);
-console.log(user2.name);
-console.log(user.name);
+// console.log(user2.name);
+// console.log(user.name);
+
+let drinks = [
+    {
+        id:1,
+        name: '紅茶',
+        price: '20'
+    },
+    {
+        id:2,
+        name: '綠茶',
+        price: '20'
+    },
+    {
+        id:3,
+        name: '奶茶',
+        price: '30'
+    }
+];
+// console.log(drinks[1].name);
+for(let drink of drinks){
+    console.log(drink.name);
+}
+drinks.forEach(function(drink){
+    console.log(drink.name);
+})
